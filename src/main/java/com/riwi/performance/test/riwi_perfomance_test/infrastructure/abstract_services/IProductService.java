413bd4a664c4732e8ee30bdd509fc9900a2626fe
mtatiana115 <mtatiana115@gmail.com>
2024-07-29 11:26:09 -0500

    fix/unnecesary methods deleted

--- src/main/java/com/riwi/performance/test/riwi_perfomance_test/infrastructure/abstract_services/IProductService.java
+++ src/main/java/com/riwi/performance/test/riwi_perfomance_test/infrastructure/abstract_services/IProductService.java
@@ -5,13 +5,11 @@ import java.util.UUID;
 import com.riwi.performance.test.riwi_perfomance_test.api.dto.request.ProductRequest;
 import com.riwi.performance.test.riwi_perfomance_test.api.dto.response.ProductResponse;
 import com.riwi.performance.test.riwi_perfomance_test.infrastructure.abstract_services.generics.CreateService;
-import com.riwi.performance.test.riwi_perfomance_test.infrastructure.abstract_services.generics.DeleteService;
 import com.riwi.performance.test.riwi_perfomance_test.infrastructure.abstract_services.generics.ReadAllService;
 import com.riwi.performance.test.riwi_perfomance_test.infrastructure.abstract_services.generics.ReadService;
-import com.riwi.performance.test.riwi_perfomance_test.infrastructure.abstract_services.generics.UpdateService;
 
 public interface IProductService extends CreateService<ProductRequest, ProductResponse>,
-    ReadService<ProductResponse, UUID>, UpdateService<ProductRequest, ProductResponse, UUID>, DeleteService<UUID>,
+    ReadService<ProductResponse, UUID>,
     ReadAllService<ProductResponse> {
 
 }

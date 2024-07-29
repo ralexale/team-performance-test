51a13ce27d135c28e842560f58ae45637998f9ad
mtatiana115 <mtatiana115@gmail.com>
2024-07-29 11:11:13 -0500

    fix/list mapper deleted

--- src/main/java/com/riwi/performance/test/riwi_perfomance_test/infrastructure/mappers/ProductMapper.java
+++ src/main/java/com/riwi/performance/test/riwi_perfomance_test/infrastructure/mappers/ProductMapper.java
@@ -1,7 +1,5 @@
 package com.riwi.performance.test.riwi_perfomance_test.infrastructure.mappers;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -17,7 +15,5 @@ public interface ProductMapper {
   Product requestToEntity(ProductRequest request);
 
   ProductResponse entityToResponse(Product product);
-
-  List<ProductResponse> productListToListResponse(List<Product> products);
 
 }
